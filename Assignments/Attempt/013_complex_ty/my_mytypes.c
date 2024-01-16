55ba16171111324e4f1ece916a17fd123e73c5ff
hsnayrus <suryanshjain.1309@gmail.com>
2024-01-16 10:52:56 -0800

    Fixed the places with syntax errors

--- Assignments/Attempt/013_complex_ty/my_mytypes.c
+++ Assignments/Attempt/013_complex_ty/my_mytypes.c
@@ -1,9 +1,9 @@
 struct _complex_num_t {
     double real_part;
     double imaginary_part;
-}
+};
 
 typedef struct _complex_num_t complex_num_t;
 
 enum _animal_t { CAT, FISH, SNAKE, DOG, HORSE };
-typedef _animal_t animal_t;
+typedef enum _animal_t animal_t;
