06c80431b7d1306516053c1583159c770b93178b
hsnayrus <suryanshjain.1309@gmail.com>
2024-01-22 10:23:22 -0800

    Wrote the code. Have to analyse for correctness now

--- Assignments/Attempt/017_squares/Assg-Files/mysquares.c
+++ Assignments/Attempt/017_squares/Assg-Files/mysquares.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 // return maximum of num1 and num2
+int max(int num1, int num2) {
+    if (num1 > num2) {
+        return num1;
+    }
+    return num2;
+}
+
 void squares(int size1, int x_offset, int y_offset, int size2) {
     // compute the max of size1 and (x_offset + size2).  Call this w
     // compute the max of size1 and (y_offset + size2).  Call this h
@@ -21,4 +28,25 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     // else print a space
     // when you finish counting x from 0 to w,
     // print a newline
+    int w = max(size1, (x_offset + size2));
+    int h = max(size1, (y_offset + size2));
+
+    for (int y = 0; y < h; y++) {
+        for (int x = 0; x < w; x++) {
+            if (((x >= x_offset && x <= (x_offset + size2)) &&
+                 (y == y_offset || y == y_offset + size2 - 1)) ||
+                ((y >= y_offset && y <= y_offset + size_2) &&
+                 (x == x_offset || x == x_offset + size2 - 1))) {
+                printf("*");
+            } else {
+                if ((x < size1 && (y == 0 || y == size1 - 1)) ||
+                    (y < size1 && (x == 0 || x == size1 - 1))) {
+                    printf("#");
+                } else {
+                    printf(" ");
+                }
+            }
+        }
+        printf("\n");
+    }
 }
