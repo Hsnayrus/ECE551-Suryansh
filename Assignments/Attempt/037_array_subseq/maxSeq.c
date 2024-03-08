70e79689bd0647a65a3ef53e3ac59a1f38916695
hsnayrus <suryanshjain.1309@gmail.com>
2024-03-08 04:08:53 -0800

    Wrote the correct solution. The testcase file is wrong on line 46

--- Assignments/Attempt/037_array_subseq/maxSeq.c
+++ Assignments/Attempt/037_array_subseq/maxSeq.c
@@ -1,4 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t maxSeq(int * array, size_t n);
+/*
+{}
+0, 1, 2
+2, 1, 0
+1, 2, 1
+1, 1, 2
+*/
+size_t maxSeq(int * array, size_t n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    size_t maxSeq = 1;
+    size_t currMaxSeq = 1;
+    for (size_t i = 1; i < n; i++) {
+        if (array[i] > array[i - 1]) {
+            currMaxSeq++;
+        } else {
+            if (currMaxSeq > maxSeq) {
+                maxSeq = currMaxSeq;
+            }
+            currMaxSeq = 1;
+        }
+    }
+    if (currMaxSeq > maxSeq) {
+        maxSeq = currMaxSeq;
+    }
+    return maxSeq;
+}
