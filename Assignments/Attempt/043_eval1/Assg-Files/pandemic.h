061e19a982624c71dd32603dffa66530724743ac
hsnayrus <suryanshjain.1309@gmail.com>
2024-03-12 08:54:21 -0700

    Used clang-format on pandemic.h

--- Assignments/Attempt/043_eval1/Assg-Files/pandemic.h
+++ Assignments/Attempt/043_eval1/Assg-Files/pandemic.h
@@ -7,8 +7,8 @@
 #define MAX_NAME_LEN 64
 
 struct country_tag {
-  char name[64];
-  uint64_t population;
+    char name[64];
+    uint64_t population;
 };
 typedef struct country_tag country_t;
 
@@ -18,9 +18,7 @@ void calcRunningAvg(unsigned * data, size_t n_days, double * avg);
 
 void calcCumulative(unsigned * data, size_t n_days, uint64_t pop, double * cum);
 
-void printCountryWithMax(country_t * countries,
-                         size_t n_countries,
-                         unsigned ** data,
-                         size_t n_days);
+void printCountryWithMax(country_t * countries, size_t n_countries,
+                         unsigned ** data, size_t n_days);
 
 #endif
